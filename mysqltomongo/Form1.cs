f0878a039d4e82f8c5452c5d224dcd47f1bc2658
mejwaller <martin.waller@asglh-wl-08471.kingsway.asos.com>
2016-04-22 14:02:43 +0100

    Insert all record events

--- mysqltomongo/Form1.cs
+++ mysqltomongo/Form1.cs
@@ -11,16 +11,22 @@ using System.Windows.Forms;
 using MySql.Data;
 using MySql.Data.MySqlClient;
 
+using MongoDB.Bson;
+using MongoDB.Driver; 
+
 namespace mysqltomongo
 {
     public partial class Form1 : Form
     {
 
         MySqlConnection conn;
-        string hostname;
-        string db;
-        string username;
-        string password; 
+        MongoClient mong;
+        //MongoServer mongserver;
+        IMongoDatabase mongdb;
+        string mysqlhostname;
+        string mysqldb;
+        string mysqlusername;
+        string mysqlpassword; 
 
         public Form1()
         {
@@ -30,21 +36,29 @@ namespace mysqltomongo
         private void button1_Click(object sender, EventArgs e)
         {
 
-            hostname = hostname_entry.Text;
-            db = db_entry.Text;
-            username = username_entry.Text;
-            password = passwd_entry.Text;
+            mysqlhostname = hostname_entry.Text;
+            mysqldb = db_entry.Text;
+            mysqlusername = username_entry.Text;
+            mysqlpassword = passwd_entry.Text;
 
             //Console.WriteLine("Hostname: " + hostname + " Database: " + db + " Username: " + username + "Password: " + password);
 
-            string connStr = "server=" + hostname+";user=" + username + ";database=" + db + ";password=" + password +";";
+            string connStr = "server=" + mysqlhostname+";user=" + mysqlusername + ";database=" + mysqldb + ";password=" + mysqlpassword +";";
 
             conn = new MySqlConnection(connStr);
+            mong = new MongoClient();
+            //mongserver = mong.GetServer();
+
             try
             {
                 Console.WriteLine("Connecting to MySQL...");
                 conn.Open();
-                Console.WriteLine("Connected successfully");
+                Console.WriteLine("Connected to MySQL successfully");
+                Console.WriteLine("Connecting to MongodDB..");
+                //mong.GetDatabase("mongmoths");
+                mongdb = mong.GetDatabase("test2");
+
+                Console.WriteLine("Connected to MongoDB successfully");
                 getRecords();
             }
             catch(Exception ex)
@@ -62,7 +76,7 @@ namespace mysqltomongo
 
         private void getRecords()
         {
-
+            //Doesn't select empty record events! (i.e. dates where trap was run but nothing caught) 
             string query = "select t.vernacular_name,t.order_name,t.family_name,t.subfamily_name,t.genus_name,t.species_name,t.subspecies_name,t.aberration_name,t.form_name,"
                 + "e.record_date,e.record_type,e.grid_ref,e.notes,"
                 + "r.count, r.notes "
@@ -70,16 +84,115 @@ namespace mysqltomongo
                 + "WHERE t.id=r.taxon_id "
                 + "AND r.recevent_id = e.event_id;";
 
-            MySqlCommand cmd = new MySqlCommand(query, conn);
+            string recev_query = "select * from record_event;";
+
+            /*
+             var doc = new BsonDocument
+             {
+                {"date", new DateTime(YYYY,MM,DD,0,0,0, DateTimeKind.Utc)}, //from e.record_date,
+                {"type", "descriptive string"}, //from e.record_type
+                {"grid_ref", "grid ref string"}, //from e.grid_ref, may not be present
+                {"notes", "notes string"}, //from e.record_notes, may not be present
+                {"species", new BsonArray //entry for each species caught, may be empty for dates where nohting was caught
+                    {
+                        new BsonDocument
+                        {
+                            {"vernacular name", "common name string"},//from t.vernacular_name - may not be present
+                            {"order","order string"},//from t.order_name
+                            {"family","family string"}, //from t.family_name
+                            {"sub-family","subfamily string"}, //from t.subfamily_name - may not be present
+                            {"genus","genus string"}, //from t.genus_name
+                            {"species","species string"}, //from t.species_name
+                            {"sub-species","subspeciees string"}, //from t.subspecies_name - may not be present
+                            {"aberration","aberration string"}, //from t.aberration_name - may not be present
+                            {"form","form string"}, //from t.form_name - may not be present
+                            {"count", count }, //from r.count
+                            {"notes","notes string"} from r.notes - may not be present
+                        },
+                        new BsonDocument
+                        {
+                            ...etc
+                        }
+                    }
+                }
+            }
+            */          
+                
+
+            MySqlCommand cmd = new MySqlCommand(recev_query, conn);
 
             MySqlDataReader dataReader = cmd.ExecuteReader();
 
-            dataReader.Read();
+            List<int> recevent_ids = new List<int>();
 
-            //dataReader.
-            for (var i = 0; i < dataReader.FieldCount; i++)
-            {
-                Console.WriteLine(dataReader[i]);
+            var collection = mongdb.GetCollection<BsonDocument>("records");
+
+            //empty the database
+            var empty = new BsonDocument();
+            collection.DeleteMany(empty);
+
+            while (dataReader.Read())
+            {                
+
+                int id_as_int = 0;
+
+                bool isInt = int.TryParse(dataReader[0].ToString(), out id_as_int);
+
+                if(isInt)
+                {
+                    recevent_ids.Add(id_as_int);
+                }
+                else
+                {
+                    Console.WriteLine("ERROR: Cannot convert record event id " + dataReader[0] + " to an int!");
+                    return;
+                }
+
+                string date = dataReader[1].ToString();
+
+                DateTime dt;
+                // Specify exactly how to interpret the string.
+                IFormatProvider culture = new System.Globalization.CultureInfo("en-GB", true);
+                
+                try 
+                {
+                    dt = Convert.ToDateTime(date);
+                    Console.WriteLine("Year: {0}, Month: {1}, Day: {2}",dt.Year, dt.Month, dt.Day);
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("ERROR: Couldn't convert {0} to a DateTime",dataReader[1]);
+                    return;
+                }
+                              
+                
+                //dataReader.
+                for (var i = 0; i < dataReader.FieldCount; i++)
+                {
+                    
+                    if(dataReader[i].ToString().Length > 0){ 
+                        Console.WriteLine(dataReader.GetName(i) + " : " + dataReader[i]);
+                    }                    
+                }
+
+                BsonDocument doc = new BsonDocument {
+                        {"Date",dt},
+                        {"Type",dataReader[2].ToString()},
+                        {"Grid Ref",dataReader[3].ToString()},
+                        {"Notes",dataReader[4].ToString()}
+                    };
+
+                Console.WriteLine(doc.ToString());
+
+                collection.InsertOne(doc);
+                //await collection.InsertOneAsync(doc);
+
+                /*foreach(int item in recevent_ids)
+                {
+                    Console.WriteLine(item.ToString());
+                }*/
+
+
             }
 
             dataReader.Close();

4bd9d9a12e1382c2414a5a437d121070d7474c37
mejwaller <martin.waller@asglh-wl-08471.kingsway.asos.com>
2016-04-26 12:23:32 +0100

    Complete code

--- mysqltomongo/Form1.cs
+++ mysqltomongo/Form1.cs
@@ -123,7 +123,7 @@ namespace mysqltomongo
 
             MySqlDataReader dataReader = cmd.ExecuteReader();
 
-            List<int> recevent_ids = new List<int>();
+            List<string> recevent_ids = new List<string>();
 
             var collection = mongdb.GetCollection<BsonDocument>("records");
 
@@ -134,7 +134,7 @@ namespace mysqltomongo
             while (dataReader.Read())
             {                
 
-                int id_as_int = 0;
+                /*int id_as_int = 0;
 
                 bool isInt = int.TryParse(dataReader[0].ToString(), out id_as_int);
 
@@ -146,7 +146,9 @@ namespace mysqltomongo
                 {
                     Console.WriteLine("ERROR: Cannot convert record event id " + dataReader[0] + " to an int!");
                     return;
-                }
+                }*/
+
+                recevent_ids.Add(dataReader[0].ToString());
 
                 string date = dataReader[1].ToString();
 
@@ -175,11 +177,15 @@ namespace mysqltomongo
                     }                    
                 }
 
+                bool haveGridRef = dataReader[3].ToString().Length > 0;
+                bool haveNotes = dataReader[4].ToString().Length > 0;
+
                 BsonDocument doc = new BsonDocument {
                         {"Date",dt},
                         {"Type",dataReader[2].ToString()},
-                        {"Grid Ref",dataReader[3].ToString()},
-                        {"Notes",dataReader[4].ToString()}
+                        {"Grid Ref",dataReader[3].ToString(),haveGridRef},
+                        {"Notes",dataReader[4].ToString(),haveNotes},
+                        {"MySQL ID",dataReader[0].ToString()}
                     };
 
                 Console.WriteLine(doc.ToString());
@@ -196,6 +202,69 @@ namespace mysqltomongo
             }
 
             dataReader.Close();
+
+            //Now, for ech record event, select all relevant records and add them to record event as a BSonArray:
+
+            foreach (var revent in recevent_ids)
+            {
+
+                string rec_query = "select t.vernacular_name,t.order_name,t.family_name,t.subfamily_name,t.genus_name,t.species_name,t.subspecies_name,t.aberration_name,t.form_name,"
+                    + "r.count, r.notes "
+                    + "FROM taxon_data AS t, record_data AS r "
+                    + "WHERE t.id=r.taxon_id "
+                    + "AND r.recevent_id = " + revent + ";";
+
+                cmd = new MySqlCommand(rec_query, conn);
+
+                dataReader = cmd.ExecuteReader();
+
+                var filter = Builders<BsonDocument>.Filter.Eq("MySQL ID", revent.ToString());
+
+                var mongres = collection.Find(filter).ToList();
+
+                Console.WriteLine(mongres[0].ToString());
+
+                BsonArray recs_array = new BsonArray();
+
+                while (dataReader.Read())
+                {
+                    Console.WriteLine("Species: {0} Count: {1}", dataReader[0], dataReader[9]);
+
+                    bool haveVernacular = dataReader[0].ToString().Length > 0;
+                    bool haveSubFamily = dataReader[3].ToString().Length > 0;
+                    bool haveSubSpecies = dataReader[6].ToString().Length > 0;
+                    bool haveAb = dataReader[7].ToString().Length > 0;
+                    bool haveForm = dataReader[8].ToString().Length > 0;
+                    bool hasNotes = dataReader[10].ToString().Length > 0;
+
+                    BsonDocument spec_doc = new BsonDocument
+                    {
+                        {"Vernacular name",dataReader[0].ToString(),haveVernacular},
+                        {"Order",dataReader[1].ToString()},
+                        {"Family",dataReader[2].ToString()},
+                        {"Subfamily",dataReader[3].ToString(),haveSubFamily},
+                        {"Genus",dataReader[4].ToString()},
+                        {"Species",dataReader[5].ToString()},
+                        {"Subspecies",dataReader[6].ToString(),haveSubSpecies},
+                        {"Aberration",dataReader[7].ToString(),haveAb},
+                        {"Form",dataReader[8].ToString(),haveForm},
+                        {"Count",dataReader[9].ToString()},
+                        {"Notes",dataReader[1].ToString(),hasNotes}
+                    };
+
+                    recs_array.Add(spec_doc);
+                }
+
+                mongres[0].Add("records", recs_array);
+
+                Console.WriteLine("recsarray: {0}", recs_array.ToString());
+                Console.WriteLine("record with array: {0}", mongres[0].ToString());
+
+                //needs to be an update...set filter, etc.
+                collection.ReplaceOne(filter, mongres[0]);
+
+                dataReader.Close();
+            }
             
 
             /*
